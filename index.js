4b19fe54316e8576f2bcf726744583e5a4b008c0
scottbooma <scottbooma@gmail.com>
2022-04-18 16:44:43 -0400

    Add random queen generation

---
+++ index.js
@@ -0,0 +1,62 @@
+getQueenData().then(randomQueenInfo => {
+    console.log(randomQueenInfo)
+    createQueenInfo(randomQueenInfo)
+})
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * max)
+}
+
+async function getListOfQueens() {
+    try {
+        const response = await fetch("http://www.nokeynoshade.party/api/queens/all")
+        const list = await response.json()
+        return list
+
+    }
+    catch (error) {
+        console.error(error.message)
+    }
+}
+
+async function getRandomQueen() {
+    try {
+        const queenList = await getListOfQueens()
+        const queen = await queenList[getRandomNumber((queenList.length - 1))]
+        return queen
+    }
+    catch (error) {
+        console.error(error.message)
+    }
+}
+
+async function getQueenData() {
+    try {
+        const randomQueen = await getRandomQueen()
+        const request = await fetch(`http://www.nokeynoshade.party/api/queens/${randomQueen.id}`)
+        const randomQueenData = await request.json()
+        return randomQueenData
+    }
+    catch (error) {
+        console.error(error.message)
+    }
+}
+
+function getQueenSeasons(queen) {
+    return queen.seasons.map(season => season.seasonNumber).join(", ")
+}
+
+function createQueenInfo(queen) {
+    const queenInfoContainer = document.querySelector("#queen")
+    queenInfoContainer.innerHTML = `
+        <h2>${queen.name}</h2>
+        <img src="${queen.image_url}" alt="${queen.name}">
+        <p>${queen.quote}</p>
+        <p>Season(s): ${getQueenSeasons(queen)}</p>
+    `
+}
+
+
+
+
+
